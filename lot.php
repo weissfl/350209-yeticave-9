6f7d3d1e27a491c96d388d1f0e01b2a70cf4a27a
weissfl <dmitry.pr@gmail.com>
2019-05-01 15:50:13 +0300

    Вывод 404 ошибки

--- lot.php
+++ lot.php
@@ -1,17 +1,24 @@
 <?php
-require('helpers.php');
 require('functions.php');
 
 $categories = getCategories();
+$lot = getPage($_GET['id']);
 
-$page_content = include_template('lot.php', []);
+if($lot !== NULL) {
+    $page_content = include_template('lot.php', ['lot' => $lot]);
+}
+else {
+    http_response_code(404);
+
+    $page_content = include_template('404.php', []);
+}
 
 $layout_content = include_template('layout.php', [
     'categories' => $categories,
     'content' => $page_content,
     'is_auth' => $is_auth,
     'user_name' => $user_name,
-    'title' => 'Yeti Cave - Главная страница'
+    'title' => ((isset($lot['name'])) ? $lot['name'] : '404 страница не найдена') . ' - Yeti Cave'
 ]);
 
 echo $layout_content;
